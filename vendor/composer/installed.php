847c22328ba5cb19ed90b5039be3707a1556e662
2197200872-TerryEdyHartanto <2197200872@student.stmik-aub.ac.id>
2022-04-25 08:05:22 +0700

    Upload Perdana

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'lenovo/tugas-modul-7',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lenovo/tugas-modul-7' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
